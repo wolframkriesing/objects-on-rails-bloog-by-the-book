8c20b71219e8d62455f88b435688c5b585b6b555
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-07 20:10:13 +0100

    Add the post_source.

--- bloog/app/models/blog.rb
+++ bloog/app/models/blog.rb
@@ -1,12 +1,21 @@
 class Blog
   attr_reader :entries
+  attr_writer :post_source
+
   def initialize
     @entries = []
   end
+
   def title
     "Watching Paint Dry"
   end
+
   def subtitle
     "The trusted source for drying paint news & opinions"
+  end
+
+  private
+  def post_source
+    @post_source ||= Post.public_method(:new)
   end
 end
