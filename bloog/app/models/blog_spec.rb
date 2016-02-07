1568f1f014246177c770ae5a45ea655a342b40f9
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-07 23:50:55 +0100

    Rename subject to blog, which is more explicit and understandable.

--- bloog/app/models/blog_spec.rb
+++ bloog/app/models/blog_spec.rb
@@ -3,28 +3,28 @@ require_relative "./blog"
 # require "ostruct"
 
 describe Blog do
-  subject { Blog.new }
+  let(:blog) { Blog.new }
 
   it "has no entries" do
-    subject.entries.must_be_empty
+    blog.entries.must_be_empty
   end
 
   describe "#new_post" do
     before do
       @new_post = OpenStruct.new
-      subject.post_source = -> { @new_post }
+      blog.post_source = -> { @new_post }
     end
     it "returns a new post" do
-      subject.new_post.must_equal @new_post
+      blog.new_post.must_equal @new_post
     end
     it "sets the post’s blog reference to itself" do
-      subject.new_post.blog.must_equal(subject)
+      blog.new_post.blog.must_equal(blog)
     end
     it "accepts an attribute hash on behalf of the post maker" do
       post_source = MiniTest::Mock.new
       post_source.expect(:call, @new_post, [{ x: 42, y: "z" }])
-      subject.post_source = post_source
-      subject.new_post(x: 42, y: "z")
+      blog.post_source = post_source
+      blog.new_post(x: 42, y: "z")
       post_source.verify
     end
   end
@@ -32,8 +32,8 @@ describe Blog do
   describe "#add_entry" do
     it "adds the entry to the blog" do
       entry = Object.new
-      subject.add_entry(entry)
-      subject.entries.must_include(entry)
+      blog.add_entry(entry)
+      blog.entries.must_include(entry)
     end
   end
 end
