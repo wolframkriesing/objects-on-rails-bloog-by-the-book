1bbc9f800ebb7a80992e40dc70e8341a4a124e38
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-07 23:46:17 +0100

    Use let instead of before.

--- bloog/app/models/post_spec.rb
+++ bloog/app/models/post_spec.rb
@@ -2,37 +2,36 @@ require "minitest/autorun"
 require_relative "./post"
 
 describe Post do
-  before do
-    @it = Post.new
-  end
+  let(:post) { Post.new }
+  
   it "starts with blank attributes" do
-    @it.title.must_be_nil 
-    @it.body.must_be_nil
+    post.title.must_be_nil 
+    post.body.must_be_nil
   end
   it "supports reading and writing a title" do
-    @it.title = "foo"
-    @it.title.must_equal "foo"
+    post.title = "foo"
+    post.title.must_equal "foo"
   end
   it "supports reading and writing a post body" do
-    @it.body = "foo"
-    @it.body.must_equal "foo"
+    post.body = "foo"
+    post.body.must_equal "foo"
   end
   it "supports reading and writing a blog reference" do
     blog = Object.new
-    @it.blog = blog
-    @it.blog.must_equal blog
+    post.blog = blog
+    post.blog.must_equal blog
   end
   describe "#publish" do
     before do
       @blog = MiniTest::Mock.new
-      @it.blog = @blog
+      post.blog = @blog
     end
     after do
       @blog.verify
     end
     it "adds the post to the blog" do
-      @blog.expect :add_entry, nil, [@it] 
-      @it.publish
+      @blog.expect :add_entry, nil, [post] 
+      post.publish
     end
   end
 end
