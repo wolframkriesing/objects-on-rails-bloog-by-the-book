e8422fb69f3bc30e2102a00761b89ec9ad0eea0b
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-07 23:46:41 +0100

    Make new_post accept a hash.

--- bloog/app/models/blog.rb
+++ bloog/app/models/blog.rb
@@ -6,12 +6,15 @@ class Blog
     @entries = []
   end
 
-  def new_post
-    post_source.call.tap do |p|
+  def new_post(*args)
+    post_source.call(*args).tap do |p|
       p.blog = self
     end
+      # post_source.call.tap do |p|
+      #   p.blog = self
+      # end
   end
-  
+
   def add_entry(entry)
     entries << entry
   end
--- bloog/app/models/blog_spec.rb
+++ bloog/app/models/blog_spec.rb
@@ -20,12 +20,19 @@ describe Blog do
     it "sets the post’s blog reference to itself" do
       subject.new_post.blog.must_equal(subject)
     end
+    it "accepts an attribute hash on behalf of the post maker" do
+      post_source = MiniTest::Mock.new
+      post_source.expect(:call, @new_post, [{ x: 42, y: "z" }])
+      subject.post_source = post_source
+      subject.new_post(x: 42, y: "z")
+      post_source.verify
+    end
   end
 
   describe "#add_entry" do
     it "adds the entry to the blog" do
-      entry = Object.new 
-      subject.add_entry(entry) 
+      entry = Object.new
+      subject.add_entry(entry)
       subject.entries.must_include(entry)
     end
   end
