064fc4ad5dc57ec2b10fe97945ef5396d7587694
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-11 17:06:19 +0100

    Make the new-form render without errors.

--- bloog/app/models/post.rb
+++ bloog/app/models/post.rb
@@ -1,10 +1,17 @@
 class Post
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+
   attr_accessor :blog, :title, :body
 
   def initialize(attrs={})
-    attrs.each do |k,v| 
-      send("#{k}=",v)
+    attrs.each do |k, v|
+      send("#{k}=", v)
     end
+  end
+
+  def persisted?
+    false
   end
 
   def publish
