1d3d35e9dadfa0add434d353008a9996d4886eb2
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-02-14 13:24:04 +0100

    Some auto-formatting.

--- bloog/app/models/post_spec.rb
+++ bloog/app/models/post_spec.rb
@@ -3,17 +3,17 @@ require "minitest/autorun"
 # this piece is also just added as in the PostController
 # only for form to render :(
 # for-form-only START
-require_relative "../helpers/spec_helper_lite" 
-stub_module "ActiveModel::Conversion" 
+require_relative "../helpers/spec_helper_lite"
+stub_module "ActiveModel::Conversion"
 stub_module "ActiveModel::Naming"
 # for-form-only END
 require_relative "./post"
 
 describe Post do
   let(:post) { Post.new }
-  
+
   it "starts with blank attributes" do
-    post.title.must_be_nil 
+    post.title.must_be_nil
     post.body.must_be_nil
   end
   it "supports reading and writing a title" do
@@ -29,8 +29,8 @@ describe Post do
     post.blog = blog
     post.blog.must_equal blog
   end
-  it "supports setting attributes in the initializer" do 
-    post = Post.new(title: "mytitle", body: "mybody") 
+  it "supports setting attributes in the initializer" do
+    post = Post.new(title: "mytitle", body: "mybody")
     post.title.must_equal "mytitle"
     post.body.must_equal "mybody"
   end
@@ -43,7 +43,7 @@ describe Post do
       @blog.verify
     end
     it "adds the post to the blog" do
-      @blog.expect :add_entry, nil, [post] 
+      @blog.expect :add_entry, nil, [post]
       post.publish
     end
   end
